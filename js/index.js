0be9ba9f73783a73d66c8bbe05ddbb77c587d734
Nwigberi Moses <70588071+TheMoeEntity@users.noreply.github.com>
2024-10-18 11:28:18 +0100

    laid out sidebar and some assets

---
+++ js/index.js
@@ -0,0 +1,5 @@
+// Toggle sidebar visibility in mobile view
+document.getElementById("toggleSidebar").addEventListener("click", function () {
+  const sidebar = document.getElementById("sidebar");
+  sidebar.classList.toggle("visible");
+});
