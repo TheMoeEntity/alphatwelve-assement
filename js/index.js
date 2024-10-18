ea2713c80093c52fee52c7170974ac79d20c46a5
Nwigberi Moses <70588071+TheMoeEntity@users.noreply.github.com>
2024-10-18 13:58:14 +0100

    "Updated CSS, HTML, and JavaScript files to modify sidebar, navigation, and card components, and added mobile footer navigation"

--- js/index.js
+++ js/index.js
@@ -1,5 +1,16 @@
 // Toggle sidebar visibility in mobile view
+const closeBtn = document.querySelector('#closeBtn-side')
+closeBtn.addEventListener('click',()=> {
+  sidebar.classList.toggle("visible");
+})
 document.getElementById("toggleSidebar").addEventListener("click", function () {
   const sidebar = document.getElementById("sidebar");
   sidebar.classList.toggle("visible");
+});
+document.getElementById('collapseBtn').addEventListener('click', function() {
+  const sidebar = document.getElementById('sidebar');
+  const content = document.querySelector('.content');
+  
+  sidebar.classList.toggle('collapsed');
+  content.classList.toggle('collapsed');
 });
